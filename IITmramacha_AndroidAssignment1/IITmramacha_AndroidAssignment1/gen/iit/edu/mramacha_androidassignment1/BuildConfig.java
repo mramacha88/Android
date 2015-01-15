7f49ada9785f8c7466e3760761096c23abf23f02
mramacha88 <meghashree.mr@gmail.com>
2015-01-14 19:29:37 -0600

    deleted

--- IITmramacha_AndroidAssignment1/IITmramacha_AndroidAssignment1/gen/iit/edu/mramacha_androidassignment1/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package iit.edu.mramacha_androidassignment1;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
